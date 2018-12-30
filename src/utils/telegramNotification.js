7fbb19b000689e6155b70a032798de47ef0dff7b
bitcoinerS <bitcoiner@cryptomoon.com>
2018-12-30 15:32:12 +0700

    alert update

--- src/utils/telegramNotification.js
+++ src/utils/telegramNotification.js
@@ -1,7 +1,7 @@
 const https = require('https');
 
 function sendTelegramNotification(feedData) {
-	let url = 'https://alert.eosdetroit.io?alert=' + encodeURIComponent(feedData.title + " " + feedData.link);
+	let url = 'https://alert.eosdetroit.io?alert=' + encodeURIComponent("Github Alert: " + feedData.title + " " + feedData.link);
 	https.get(url, (resp) => {
 	}).on("error", (err) => {
   		console.log("Error: " + err.message);
