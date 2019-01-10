9ef2c4805a554605e30688a800589daa08696da0
bitcoinerS <bitcoiner@cryptomoon.com>
2019-01-10 13:54:22 +0700

    replaced telegram alert

--- src/utils/telegramNotification.js
+++ src/utils/telegramNotification.js
@@ -1,8 +1,11 @@
 const https = require('https');
 
 function sendTelegramNotification(feedData) {
-	let url = 'https://alert.eosdetroit.io?alert=' + encodeURIComponent("Github Alert: " + feedData.title + " " + feedData.link);
-	https.get(url, (resp) => {
+	let TELEGRAM_KEY = "552780825:AAFGfxVWcACZ5vKrL1NmaTCDU6xpzCKCh0Q";
+	let TELEGRAM_CHAT_I D= "-1001401965082";
+	let message = encodeURIComponent("Github Alert: " + feedData.title + " " + feedData.link);
+	let TELEGRAM_URL = "https://api.telegram.org/bot" + TELEGRAM_KEY + "/sendMessage?chat_id=" + TELEGRAM_CHAT_ID + "&text=" + message;
+	https.get(TELEGRAM_URL, (resp) => {
 	}).on("error", (err) => {
   		console.log("Error: " + err.message);
 	});
