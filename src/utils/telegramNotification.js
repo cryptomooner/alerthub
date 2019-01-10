ae588107d196d566f124c3b7b030296702b5ada0
bitcoinerS <bitcoiner@cryptomoon.com>
2019-01-10 13:57:56 +0700

    wip

--- src/utils/telegramNotification.js
+++ src/utils/telegramNotification.js
@@ -2,7 +2,7 @@ const https = require('https');
 
 function sendTelegramNotification(feedData) {
 	let TELEGRAM_KEY = "552780825:AAFGfxVWcACZ5vKrL1NmaTCDU6xpzCKCh0Q";
-	let TELEGRAM_CHAT_I D= "-1001401965082";
+	let TELEGRAM_CHAT_ID = "-1001401965082";
 	let message = encodeURIComponent("Github Alert: " + feedData.title + " " + feedData.link);
 	let TELEGRAM_URL = "https://api.telegram.org/bot" + TELEGRAM_KEY + "/sendMessage?chat_id=" + TELEGRAM_CHAT_ID + "&text=" + message;
 	https.get(TELEGRAM_URL, (resp) => {
